6a0f041ce7822909e7cdb758b509ccafc7a8e9e7
Alex <polak.uzh@gmail.com>
2020-09-14 18:32:31 +0300

    qq

--- js/tiny-slider.js
+++ js/tiny-slider.js
@@ -3015,39 +3015,22 @@ var tns = (function (){
                     if (moveDirectionExpected === '?') { moveDirectionExpected = getMoveDirectionExpected(); }
                     if (moveDirectionExpected) { preventScroll = true; }
                 }
-
-                if ((typeof e.cancelable !== 'boolean' || e.cancelable) && preventScroll) {
-                    e.preventDefault();
-                }
             }
         }
 
         function panUpdate (e) {
-            if (!moveDirectionExpected) {
-                panStart = false;
-                return;
-            }
             caf(rafIndex);
-            if (panStart) { rafIndex = raf(function(){ panUpdate(e); }); }
+            if (panStart) {
+                rafIndex = raf(function(){ panUpdate(e); });
+            }
 
             if (moveDirectionExpected === '?') { moveDirectionExpected = getMoveDirectionExpected(); }
             if (moveDirectionExpected) {
-                if (!preventScroll && isTouchEvent(e)) { preventScroll = true; }
-
-                try {
-                    if (e.type) { events.emit(isTouchEvent(e) ? 'touchMove' : 'dragMove', info(e)); }
-                } catch(err) {}
-
                 var x = translateInit,
                     dist = getDist(lastPosition, initPosition);
-                if (!horizontal || fixedWidth || autoWidth) {
-                    x += dist;
-                    x += 'px';
-                } else {
                     var percentageX = TRANSFORM ? dist * items * 100 / ((viewport + gutter) * slideCountNew): dist * 100 / (viewport + gutter);
                     x += percentageX;
                     x += '%';
-                }
 
                 container.style[transformAttr] = transformPrefix + x + transformPostfix;
             }
